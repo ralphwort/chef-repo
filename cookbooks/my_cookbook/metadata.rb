aed4cb34486992f7c798ca654e2295fc61a61adc
ralph <ralph@workstation.rwort.co.uk>
2014-05-23 17:36:10 +0100

    1

--- cookbooks/my_cookbook/metadata.rb
+++ cookbooks/my_cookbook/metadata.rb
@@ -7,7 +7,8 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'build-essential'
+depends 'chef-client'
 depends 'apache2', '>= 1.0.4'
-depends 'chef_client', '>= 0.0.0'
+depends 'chef_client', '>= 1.0.0'
 depends 'apt', '>= 1.0.0'
 depends 'ntp', '>= 1.0.0'
