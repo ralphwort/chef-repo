a8b1e2adac0b2b7b5c04512069f75770f8f79e98
root <root@workstation.rwort.co.uk>
2014-05-23 14:06:36 +0100

    after fiddling with berks that didnt work

--- cookbooks/my_cookbook/metadata.rb
+++ cookbooks/my_cookbook/metadata.rb
@@ -5,3 +5,9 @@ license          'All rights reserved'
 description      'Installs/Configures my_cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'build-essential'
+depends 'apache2', '>= 1.0.4'
+depends "chef_client"
+depends "apt"
+depends "ntp"
--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -6,3 +6,6 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "chef-client"
+include_recipe "apt"
+include_recipe "ntp"
