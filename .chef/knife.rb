d0b9b6154ee272c1cdb1969cd1dd005d2eeb69b5
ralph <ralph@workstation.rwort.co.uk>
2014-05-23 13:06:19 +0100

    just before import 1

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -7,3 +7,6 @@ validation_key           '/home/ralph/chef-repo/.chef/chef-validator.pem'
 chef_server_url          'https://chef.rwort.co.uk:443'
 syntax_check_cache_path  '/home/ralph/chef-repo/.chef/syntax_check_cache'
 cookbook_path [ '/home/ralph/chef-repo/cookbooks' ]
+cookbook_copyright "Ralph Wort"
+cookbook_license none
+cookbook_email "ralph@rwort.co.uk"
