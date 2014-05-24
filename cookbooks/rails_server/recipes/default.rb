93d5f7c77efbea3f68ac72d3fd1e442045a4a0eb
ralph <ralph@workstation.rwort.co.uk>
2014-05-24 14:04:23 +0100

    with passenger and unicorn

--- cookbooks/rails_server/recipes/default.rb
+++ cookbooks/rails_server/recipes/default.rb
@@ -7,11 +7,22 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "passenger_apache2"
 include_recipe "curl"
 include_recipe "rvm"
+include_recipe "passenger_apache2"
+include_recipe "unicorn"
+
+execute "install rails" do
+  command "rvm install ruby"
+  command "rvm use ruby --default"
+  command "rvm rubygems current"
+  command "gem install rails"
+  user "root"
+end
 
 execute "touch rw" do
   command "touch /tmp/rw.tmp"
   user "root"
 end
+
+include_recipe "passenger"
