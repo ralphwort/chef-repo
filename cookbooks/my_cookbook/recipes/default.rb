b04bfe83aada38dfa9e7ddbd0a7dfdab06356264
ralph <ralph@workstation.rwort.co.uk>
2014-05-24 11:31:32 +0100

    one action in my_cookbook to touch /tmp/rw.tmp

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -9,5 +9,10 @@
 include_recipe "chef-client"
 include_recipe "apt"
 include_recipe "ntp"
-include_recipe "build-essentials"
+include_recipe "build-essential"
 include_recipe "apache2"
+
+execute "touch rw" do
+  command "touch /tmp/rw.tmp"
+  user "root"
+end
